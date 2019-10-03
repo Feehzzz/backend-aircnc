a884440c35bcf294195aa5a147d99dba52e7fb28
Fernando Silva <fehshuffle@gmail.com>
2019-10-03 19:06:21 -0300

    adicionado morgan para verificação de req http e adicionado campo virtual no model de spots

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
+const path = require('path');
+const app = express();
+
 
 mongoose.connect(`mongodb+srv://${process.env.db_user}:${process.env.db_pass}@${process.env.db_host}`,{ 
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-const app = express();
-
-
+app.use('/files', express.static(path.resolve(__dirname, '..','uploads')));
+app.use(morgan('tiny'))
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 app.listen(port, () => {
-  console.log('Up')
+  console.log('Connection established')
 })
--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -4,7 +4,9 @@ module.exports = {
  async store(req,res){
     const { email } = req.body;
     let user = await User.findOne({ email });
-
+    if(!email){
+      return res.status(400).json({ error: "Error during requisition"})
+    }
     if(!user){
       user = await User.create({ email });
     }
